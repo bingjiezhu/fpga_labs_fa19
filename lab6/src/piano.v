d0b63b7d6ace99302bb62acea665a55b707298f8
bingjiezhu <53612653+bingjiezhu@users.noreply.github.com>
2019-10-19 12:50:29 -0700

    Update piano.v

--- lab6/src/piano.v
+++ lab6/src/piano.v
@@ -6,33 +6,36 @@ module piano #(
 
     input [2:0] buttons,
     input [1:0] switches,
-    output [5:0] leds,
+    output reg [5:0] leds,
 
-    output [7:0] ua_tx_din,
-    output ua_tx_wr_en,
+    output reg [7:0] ua_tx_din,
+    output reg ua_tx_wr_en,
     input ua_tx_full,
 
     input [7:0] ua_rx_dout,
     input ua_rx_empty,
-    output ua_rx_rd_en,
+    output reg ua_rx_rd_en,
 
     output [23:0] tone,
     output volume
 );
-	assign volume = 1; 
-	parameter S0 = 2'b00; // IDLE  
-	parameter S1 = 2'b01; // RECEIVE 
-	parameter S2 = 2'b10; // TRANSMIT 
-	parameter S3 = 2'b11; // PLAYING
+	assign volume = switches[1]; 
+	localparam S0 = 2'b00; // IDLE  
+	localparam S1 = 2'b01; // RECEIVE 
+	localparam S2 = 2'b10; // TRANSMIT 
+	localparam S3 = 2'b11; // PLAYING
 
 
 	wire [7:0] last_address;
-	reg [7:0] address_reg;
+	reg [7:0] address_reg =0;
 	reg [7:0] address;
-	reg [31:0] note_length;
+	reg [31:0] note_length =32'd25000000;
 	reg [31:0]counter;
-
-	piano_scale_rom tone_rom(
+	reg [1:0] next_state, current_state;
+	reg data_ready =0;	 // wait the fifo data
+	reg data_pass =0;  // ensure data is ready to pass
+	
+	rom tone_rom(
 	.address(address),
 	.data(tone),
 	.last_address(last_address)
@@ -46,8 +49,8 @@ module piano #(
 	next_state = S0;
 	case (current_state)
 		S0: if (!ua_rx_empty) next_state = S1; else next_state =S0;
-		S1: if (!ua_tx_full) next_state = S2; else next_state = S1;
-		S2: next_state = S3;
+		S1: if (!ua_tx_full && data_ready) next_state = S2; else next_state = S1;
+		S2: if(data_pass) next_state = S3; else next_state =S2;
 		S3:	if(counter >= note_length) next_state = S0; else next_state = S3;
 		default: next_state = S0;
 	endcase
@@ -59,20 +62,106 @@ always @(posedge clk)
 	if (rst)
 		begin 
 			counter<= 32'd0;
-			note_ <= 32'd25000000;
+			note_length<= 32'd25000000;
+			address <=0;
 		end
 	current_state <= next_state;
 	case (current_state)
-		S0: begin ua_rx_rd_en<=0; ua_tx_wr_en<=0; counter<=0; end
-		S1: begin ua_rx_rd_en<=1; ua_tx_wr_en<=0; ua_tx_din <= ua_rx_dout; address_reg <= ua_rx_dout; counter<=0; end
-		S2: begin ua_tx_wr_en<=1; ua_rx_rd_en<=0; counter <=0; end
-		S3: begin ua_rx_rd_en<=0; ua_tx_wr_en<=0; leds[0] <= 1; address <= address_reg; if(counter< note_length) counter<= counter+1; end 
-		default: begin ua_rx_rd_en<=0; ua_tx_wr_en<=0; counter<=0;end 
+		S0: begin  data_ready<=0; address<=0; leds <= 6'b110001; ua_rx_rd_en<=0; ua_tx_wr_en<=0; counter<=0; data_pass <=0;end
+		S1: begin  ua_rx_rd_en<=1; ua_tx_wr_en<=0; counter<=0; leds <= 6'b110010; data_ready <=1; data_pass <=0; if(data_ready) begin ua_rx_rd_en<=0;end  end
+		S2: begin  ua_rx_rd_en<=0; leds <= 6'b110100; if(data_pass) begin ua_tx_wr_en<=1; end ua_tx_din <= ua_rx_dout; address_reg <= ua_rx_dout; counter <=0;data_pass <=1; data_ready <=0; end
+		S3: begin ua_rx_rd_en<=0; ua_tx_wr_en<=0; leds <= 6'b111000; address <= address_reg; data_ready <=0; data_pass <=0; if(counter< note_length) counter<= counter+1; end 
+		default: begin ua_rx_rd_en<=0; ua_tx_wr_en<=0; counter<=0; data_ready <=0; data_pass <=0;end 
 	endcase
+
 // Variable note length 
 	if (switches[0] && buttons[0]) 
 			 note_length <=note_length - 32'd1000000;
-	else if (!switches[0] && buttons[0]) 
+	else if ((!switches[0] && buttons[0] ) 
+			note_length <=note_length + 32'd1000000;
+ 	else ;
+	end 
+
+endmodulemodule piano #(
+    parameter CLOCK_FREQ = 125_000_000
+) (
+    input clk,
+    input rst,
+
+    input [2:0] buttons,
+    input [1:0] switches,
+    output reg [5:0] leds,
+
+    output reg [7:0] ua_tx_din,
+    output reg ua_tx_wr_en,
+    input ua_tx_full,
+
+    input [7:0] ua_rx_dout,
+    input ua_rx_empty,
+    output reg ua_rx_rd_en,
+
+    output [23:0] tone,
+    output volume
+);
+	assign volume = switches[1]; 
+	localparam S0 = 2'b00; // IDLE  
+	localparam S1 = 2'b01; // RECEIVE 
+	localparam S2 = 2'b10; // TRANSMIT 
+	localparam S3 = 2'b11; // PLAYING
+
+
+	wire [7:0] last_address;
+	reg [7:0] address_reg =0;
+	reg [7:0] address;
+	reg [31:0] note_length =32'd25000000;
+	reg [31:0]counter;
+	reg [1:0] next_state, current_state;
+	reg data_ready =0;	 // wait the fifo data
+	reg data_pass =0;  // ensure data is ready to pass
+	
+	rom tone_rom(
+	.address(address),
+	.data(tone),
+	.last_address(last_address)
+	);
+	
+	
+
+// FSM state changing
+	always @(*)
+	begin 
+	next_state = S0;
+	case (current_state)
+		S0: if (!ua_rx_empty) next_state = S1; else next_state =S0;
+		S1: if (!ua_tx_full && data_ready) next_state = S2; else next_state = S1;
+		S2: if(data_pass) next_state = S3; else next_state =S2;
+		S3:	if(counter >= note_length) next_state = S0; else next_state = S3;
+		default: next_state = S0;
+	endcase
+	end 
+
+// FSM output
+always @(posedge clk)
+	begin 
+	if (rst)
+		begin 
+			counter<= 32'd0;
+			note_length<= 32'd25000000;
+			address <=0;
+		end
+	current_state <= next_state;
+	case (current_state)
+		S0: begin  data_ready<=0; address<=0; leds <= 6'b110001; ua_rx_rd_en<=0; ua_tx_wr_en<=0; counter<=0; data_pass <=0;end
+		S1: begin  ua_rx_rd_en<=1; ua_tx_wr_en<=0; counter<=0; leds <= 6'b110010; data_ready <=1; data_pass <=0; if(data_ready) begin ua_rx_rd_en<=0;end  end
+		S2: begin  ua_rx_rd_en<=0; leds <= 6'b110100; if(data_pass) begin ua_tx_wr_en<=1; end ua_tx_din <= ua_rx_dout; address_reg <= ua_rx_dout; counter <=0;data_pass <=1; data_ready <=0; end
+		S3: begin ua_rx_rd_en<=0; ua_tx_wr_en<=0; leds <= 6'b111000; address <= address_reg; data_ready <=0; data_pass <=0; if(counter< note_length) counter<= counter+1; end 
+		default: begin ua_rx_rd_en<=0; ua_tx_wr_en<=0; counter<=0; data_ready <=0; data_pass <=0;end 
+	endcase
+
+// Variable note length 
+	if (switches[0] && buttons[0]) 
+			 note_length <=note_length - 32'd1000000;
+	else if ((!switches[0] && buttons[0] ) 
 			note_length <=note_length + 32'd1000000;
  	else ;
 	end 
